abc6d1ee742b98c936a586d656f8f7184507d120
10cheon00 <10cheon00@gmail.com>
2024-03-01 22:48:14 +0900

    test: add import codes

--- src/test/java/com/taekcheonkim/todolist/repository/UserRepositoryTest.java
+++ src/test/java/com/taekcheonkim/todolist/repository/UserRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.taekcheonkim.todolist.repository;
 
+import com.taekcheonkim.todolist.domain.User;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
