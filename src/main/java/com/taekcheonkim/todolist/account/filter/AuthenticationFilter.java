f024efea45e58a47e3d1292f46061a7c9a3ad62a
10cheon00 <10cheon00@gmail.com>
2024-03-03 13:55:06 +0900

    fix: change AuthenticationFilter to extend OncePerRequestFilter

--- src/main/java/com/taekcheonkim/todolist/account/filter/AuthenticationFilter.java
+++ src/main/java/com/taekcheonkim/todolist/account/filter/AuthenticationFilter.java
@@ -2,13 +2,16 @@ package com.taekcheonkim.todolist.account.filter;
 
 import com.taekcheonkim.todolist.account.repository.UserRepository;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class AuthenticationFilter implements Filter {
+public class AuthenticationFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
 
     @Autowired
@@ -17,7 +20,7 @@ public class AuthenticationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
     }
 }
