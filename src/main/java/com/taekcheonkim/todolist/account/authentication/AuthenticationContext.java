cbe55ea8e8606f476ed6589d36c661d2d4dcbcf9
10cheon00 <10cheon00@gmail.com>
2024-03-05 21:23:05 +0900

    fix: add @Component to AuthenticationContext

--- src/main/java/com/taekcheonkim/todolist/account/authentication/AuthenticationContext.java
+++ src/main/java/com/taekcheonkim/todolist/account/authentication/AuthenticationContext.java
@@ -1,8 +1,10 @@
 package com.taekcheonkim.todolist.account.authentication;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 @RequestScope
+@Component
 public class AuthenticationContext {
     private AuthenticatedUserHolder authenticatedUserHolder;
 
