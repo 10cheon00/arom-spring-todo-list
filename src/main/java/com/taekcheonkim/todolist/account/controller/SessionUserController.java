f4ed2c7dbe51057c090df95e740f59079d1b5163
10cheon00 <10cheon00@gmail.com>
2024-03-06 19:54:42 +0900

    feat: implement SessionUserController

--- src/main/java/com/taekcheonkim/todolist/account/controller/SessionUserController.java
+++ src/main/java/com/taekcheonkim/todolist/account/controller/SessionUserController.java
@@ -1,10 +1,19 @@
 package com.taekcheonkim.todolist.account.controller;
 
+import com.taekcheonkim.todolist.account.authentication.AuthenticatedUserHolder;
 import com.taekcheonkim.todolist.account.authentication.AuthenticationManager;
 import com.taekcheonkim.todolist.account.dto.LoginDto;
+import com.taekcheonkim.todolist.account.exception.InvalidLoginFormException;
+import com.taekcheonkim.todolist.account.authentication.SessionAttributes;
 import com.taekcheonkim.todolist.account.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class SessionUserController extends UserController {
     public SessionUserController(UserService userService, AuthenticationManager authenticationManager) {
@@ -12,7 +21,16 @@ public class SessionUserController extends UserController {
     }
 
     @PostMapping("/signin")
-    public void signIn(HttpServletResponse response, LoginDto loginDto) {
-
+    public void signIn(@RequestBody LoginDto loginDto, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        AuthenticatedUserHolder authenticatedUserHolder = authenticationManager.authenticate(Optional.ofNullable(loginDto));
+        if (authenticatedUserHolder.isAuthenticated()) {
+            HttpSession httpSession = request.getSession();
+            httpSession.setAttribute(SessionAttributes.Authenticated, true);
+            httpSession.setAttribute(SessionAttributes.AuthenticatedUserHolder, authenticatedUserHolder);
+            response.sendRedirect("/");
+        }
+        else {
+            throw new InvalidLoginFormException();
+        }
     }
 }
--- src/test/java/com/taekcheonkim/todolist/account/controller/SessionUserControllerTest.java
+++ src/test/java/com/taekcheonkim/todolist/account/controller/SessionUserControllerTest.java
@@ -2,6 +2,7 @@ package com.taekcheonkim.todolist.account.controller;
 
 import com.taekcheonkim.todolist.account.authentication.AuthenticatedUserHolder;
 import com.taekcheonkim.todolist.account.authentication.AuthenticationManager;
+import com.taekcheonkim.todolist.account.authentication.SessionAttributes;
 import com.taekcheonkim.todolist.account.domain.User;
 import com.taekcheonkim.todolist.account.dto.LoginDto;
 import com.taekcheonkim.todolist.account.dto.UserFormDto;
@@ -11,7 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 
 import java.util.Optional;
 
@@ -22,7 +25,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class SessionUserControllerTest {
-    private final SessionUserController sessionUserController;
+    private SessionUserController sessionUserController;
     @Mock
     private UserService userService;
     @Mock
@@ -35,7 +38,9 @@ public class SessionUserControllerTest {
     private final LoginDto invalidLoginDto;
     private final User savedUser;
 
+    private MockHttpServletRequest request;
     private MockHttpServletResponse response;
+    private MockHttpSession httpSession;
 
     public SessionUserControllerTest() {
         String email = "email@test.com";
@@ -48,13 +53,16 @@ public class SessionUserControllerTest {
 
         this.authenticatedUserHolder = new AuthenticatedUserHolder(Optional.of(savedUser));
         this.notAuthenticatedUserHolder = new AuthenticatedUserHolder(Optional.empty());
-        this.sessionUserController = new SessionUserController(userService, authenticationManager);
     }
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        this.request = new MockHttpServletRequest();
         this.response = new MockHttpServletResponse();
+        this.httpSession = new MockHttpSession();
+        this.request.setSession(this.httpSession);
+        this.sessionUserController = new SessionUserController(userService, authenticationManager);
     }
 
 
@@ -65,9 +73,12 @@ public class SessionUserControllerTest {
         // when
         // then
         assertThatNoException().isThrownBy(() -> {
-            sessionUserController.signIn(response, loginDto);
+            sessionUserController.signIn(loginDto, request, response);
         });
         assertThat(response.getRedirectedUrl()).isNotEmpty();
+        assertThat(httpSession.getAttribute(SessionAttributes.Authenticated)).isEqualTo(true);
+        AuthenticatedUserHolder result = (AuthenticatedUserHolder) httpSession.getAttribute(SessionAttributes.AuthenticatedUserHolder);
+        assertThat(result.isAuthenticated()).isEqualTo(true);
     }
 
     @Test
@@ -76,7 +87,7 @@ public class SessionUserControllerTest {
         when(authenticationManager.authenticate(any(Optional.class))).thenReturn(notAuthenticatedUserHolder);
         // when
         // then
-        assertThatThrownBy(() -> sessionUserController.signIn(response, invalidLoginDto)).isInstanceOf(InvalidLoginFormException.class);
+        assertThatThrownBy(() -> sessionUserController.signIn(invalidLoginDto, request, response)).isInstanceOf(InvalidLoginFormException.class);
     }
 
     @Test
@@ -85,6 +96,6 @@ public class SessionUserControllerTest {
         when(authenticationManager.authenticate(any(Optional.class))).thenReturn(notAuthenticatedUserHolder);
         // when
         // then
-        assertThatThrownBy(() -> sessionUserController.signIn(response, null)).isInstanceOf(InvalidLoginFormException.class);
+        assertThatThrownBy(() -> sessionUserController.signIn(null, request, response)).isInstanceOf(InvalidLoginFormException.class);
     }
 }
